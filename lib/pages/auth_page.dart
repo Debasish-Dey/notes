91c6e53a52fb0d73e7dfd1255e7cf3512ead9f39
Debasish Dey <126506555+Debasish-Dey@users.noreply.github.com>
2024-05-05 00:55:20 +0530

    firebase linking done

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,13 @@
+//this checks if the user is Signed In or not
+//if the user is not signed in then we show the login page else redirect to home page
+
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
